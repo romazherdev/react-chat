fc6bf5a562275a56f5b63d4527700ad9b773e4b5
Raman Zherdzeu <raman_zherdzeu@epam.com>
2022-09-13 23:17:55 +0200

    Add basic interface

--- src/components/pages/chat/ChatPage.tsx
+++ src/components/pages/chat/ChatPage.tsx
@@ -1,5 +1,33 @@
+import Form from 'react-bootstrap/Form';
+
+import styles from './ChatPage.module.css';
+
 const ChatPage = (): JSX.Element => (
-    <div>ChatPage</div>
+    <div className={styles.page}>
+        <main className={styles.chat}>
+            
+        </main>
+
+        <aside className={styles.aside}>
+            <header>
+                <button>Logout</button>
+            </header>
+            <ul className={styles.memberList}>
+                <li className={styles.memberListItem}>Member 1</li>
+                <li className={styles.memberListItem}>Member 2</li>
+                <li className={styles.memberListItem}>Member 3</li>
+            </ul>
+        </aside>
+
+        <Form className={styles.inputArea}>
+            <Form.Control
+                className={styles.inputField}
+                autoComplete="off"
+                type="text"
+                placeholder="Type your message"
+            />
+        </Form>
+    </div>
 );
 
 export default ChatPage;
